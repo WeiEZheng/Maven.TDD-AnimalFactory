bff4a527472e2ab8491cb50443edaf189f882aa1
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-03-04 11:20:51 -0500

    did more tests for cat

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -1,6 +1,7 @@
 package rocks.zipcodewilmington;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Cat;
 
@@ -10,14 +11,11 @@ import java.util.Date;
  * @author leon on 4/19/18.
  */
 public class CatTest {
-    // TODO - Create tests for `void setName(String name)`
-    // TODO - Create tests for `speak`
-    // TODO - Create tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
-    // TODO - Create tests for `Integer getId()`
-    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
-    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
-
+    //Given setup
+    private String givenName1 = "CatName";
+    private Date givenBirthDate1 = new Date();
+    private Integer givenId1 = 1;
+    private Cat cat1 = new Cat(givenName1,givenBirthDate1,givenId1);
 
     @Test
     public void constructorTest() {
@@ -41,15 +39,60 @@ public class CatTest {
     }
     @Test
     public void setNameTest(){
-        // Given
-        String givenName = "CatName";
-        Date givenBirthDate = new Date();
-        Integer givenId = 1;
-        //When
-        Cat cat = new Cat(givenName,givenBirthDate,givenId);
-        cat.setName("CatNameChanged");
+        //Given
+        String givenName2 = "ChangedCatName";
 
-        String actualName = cat.getName();
-        Assert.assertEquals(givenName,actualName);
+        //When
+        cat1.setName(givenName2);
+        String actualName = cat1.getName();
+
+        // Then
+        Assert.assertEquals(givenName2,actualName);
     }
+    @Test
+    public void SpeakTest() {
+        //When
+        String actualPhrase = cat1.speak();
+
+        //Then
+        Assert.assertEquals("meow!",actualPhrase);
+    }
+    @Test
+    public void setBirthdayTest() {
+        // Given
+        Date newBirthdate = new Date(2022,1,1);
+
+        //When
+        cat1.setBirthDate(newBirthdate);
+        Date actualBirthdate = cat1.getBirthDate();
+
+        //Then
+        Assert.assertEquals(newBirthdate,actualBirthdate);
+    }
+    @Test
+    public void eatTest(){
+        // Given
+        Integer numberOfMeals=1;
+        Food food=new Food();
+
+        //When
+        cat1.eat(food);
+
+        //Then
+        Assert.assertEquals(numberOfMeals,cat1.getNumberOfMealsEaten());
+    }
+    @Test
+    public void getIDTest(){
+        //When
+        Integer actualID=cat1.getId();
+
+        //Then
+        Assert.assertEquals(givenId1,actualID);
+    }
+    @Test
+    public void checkAnimalInheritance(){
+
+    }
+    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
+    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
 }
